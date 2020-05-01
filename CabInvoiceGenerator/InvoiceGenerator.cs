75b66302615f5a4733954a02279ac1808af12886
Saksham27 <geekysaksham27@gmail.com>
2020-05-01 11:06:05 +0530

    Created Cab invoice generator project

---
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator
+    {
+        public double CalculateFare(double distance, int time)
+        {
+            return default;
+        }
+    }
+}
---
+++ InvoiceServiceTest/InvoiceServiceTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using CabInvoiceGenerator;
+
+namespace InvoiceServiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenDistanceAndTime_ShouldReturnTotalFare()
+        {
+            
+        }
+    }
+}
